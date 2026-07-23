6ca32afb5f70ce97247baa1a268466b8acb09eb8
agrotom <razoreclams@gmail.com>
2026-07-23 20:48:18 +0300

    leaflet is removed

---
+++ frontend/static/js/openlayer.js
@@ -0,0 +1,17 @@
+import Map from 'https://cdn.jsdelivr.net/npm/ol/Map.js';
+import View from 'https://cdn.jsdelivr.net/npm/ol/View.js';
+import TileLayer from 'https://cdn.jsdelivr.net/npm/ol/layer/Tile.js';
+import OSM from 'https://cdn.jsdelivr.net/npm/ol/source/OSM.js';
+
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM(),
+    }),
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 2,
+  }),
+});
---
+++ frontend/vite.config.js
@@ -0,0 +1,9 @@
+export default {
+  build: {
+    sourcemap: true,
+  },
+  server: {
+    host: "127.0.0.1",
+    port: 5175
+  }
+}
